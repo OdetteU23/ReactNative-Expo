67c41cdf7d6c207a565c8183df15f38a2b4e19cc
Odette <uweraodette01@gmail.com>
2026-02-25 13:15:13 +0200

    Expo

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Platform } from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 export default function App() {
+  console.log("Welcome to rnExpo!");
   return (
-    <View style={styles.container}>
+    <SafeAreaProvider style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaProvider>
   );
 }
 
@@ -15,6 +17,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    //justifyContent: 'center',
+      paddingTop: Platform.OS === 'android' ? 25 : 0, // Add padding for Android status bar
   },
 });
