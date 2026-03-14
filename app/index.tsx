da761e751c051b85c8c1c484fd80bb39f39ff569
Odette <uweraodette01@gmail.com>
2026-03-14 11:00:50 +0200

    comp-lib

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { View } from 'react-native';
+import SimpleComp from '@/components/SimpleComp';
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1 }}>
+      <SimpleComp />
+    </View>
+  );
+}
